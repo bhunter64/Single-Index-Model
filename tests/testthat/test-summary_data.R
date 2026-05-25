02914071b93da13ce57887b6044e75ed02a4cd97
bhunter64 <braydonhunter@gmail.com>
2026-05-25 11:29:42 -0400

    Polished sim and summary test scripts

--- tests/testthat/test-summary_data.R
+++ tests/testthat/test-summary_data.R
@@ -1,4 +1,4 @@
 test_that("summary is factual", {
-  dat <-simudat(400, beta = c(log(3), log(2), log(0.5)), gamma = c(-1.8, -2, 0.9))
+  dat <-simu_data(400, beta = c(log(3), log(2), log(0.5)), gamma = c(-1.8, -2, 0.9))
   summary_data(dat)
 })
